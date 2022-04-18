a4ce68da0d7ebe41aff47614c2cd0793d1c1b739
정대현 <eogus_2513@dsm.hs.kr>
2022-04-18 10:41:09 +0900

    redis properties 수정

--- src/main/java/com/study/test/global/redis/RedisProperties.java
+++ src/main/java/com/study/test/global/redis/RedisProperties.java
@@ -1,20 +1,17 @@
 package com.study.test.global.redis;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 
 @Getter
+@AllArgsConstructor
 @ConstructorBinding
 @ConfigurationProperties(prefix = "spring.redis")
 public class RedisProperties {
 
     private final String host;
     private final int port;
-
-    public RedisProperties(String host, int port) {
-        this.host = host;
-        this.port = port;
-    }
 
 }
